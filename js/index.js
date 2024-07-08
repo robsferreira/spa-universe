f48f207aaf54239064608d7579ed69b1be7469bd
Robson <52553115+robsferreira@users.noreply.github.com>
2024-07-08 10:29:07 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 import { Router } from './router.js';
 
 const router = new Router()
-router.add('/spa-universe/', "/pages/home.html")
-router.add('/spa-universe/ouniverso', "/pages/ouniverso.html")
-router.add('/spa-universe/exploracao', "/pages/exploracao.html")
+router.add('/', "/pages/home.html")
+router.add('/ouniverso', "/pages/ouniverso.html")
+router.add('/exploracao', "/pages/exploracao.html")
 router.add(404, "/pages/404.html")
 
 router.handle()
